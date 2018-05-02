6bde4cc6ed2ac5ed80615ad11cd8968a2b824010
Rob Garfoot <rob@garfoot.com>
2018-05-02 14:18:38 +0100

    Initial commit.

---
+++ FluentUriBuilder.UnitTests/FluentUriTests.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using FluentAssertions;
+using Garfoot.FluentUriBuilder;
+using NUnit.Framework;
+
+namespace FluentUriBuilder.UnitTests
+{
+    [TestFixture]
+    [Category("UnitTests")]
+    public class FluentUriTests
+    {
+        [Test]
+        public void SchemeAndHost_ReturnsCorrectUri()
+        {
+            var uri = FluentUriExtensions.Create()
+                                         .Scheme("http")
+                                         .Host("www.example.com")
+                                         .AsString();
+
+            uri.Should().Be("http://www.example.com");
+        }
+
+        [TestCaseSource(nameof(GetValidPathSegments))]
+        public void PathSegments_ReturnsCorrectUri(IEnumerable<string> pathSegments, string expected)
+        {
+            var uri = FluentUriExtensions.Create()
+                                         .Scheme("http")
+                                         .Host("www.example.com")
+                                         .AddPathSegment(pathSegments)
+                                         .AsString();
+
+            uri.Should().Be(expected);
+        }
+
+        [Test]
+        public void WithPortNot80_ReturnsCorrectUri()
+        {
+            var uri = FluentUriExtensions.Create()
+                                         .Scheme("http")
+                                         .Host("www.example.com")
+                                         .Port(8080)
+                                         .AsString();
+
+            uri.Should().Be("http://www.example.com:8080");
+        }
+
+        [Test]
+        public void WithPort80_ReturnsCorrectUri()
+        {
+            var uri = FluentUriExtensions.Create()
+                                         .Scheme("http")
+                                         .Host("www.example.com")
+                                         .Port(80)
+                                         .AsString();
+
+            uri.Should().Be("http://www.example.com");
+        }
+
+
+        [TestCase("aUser", null, "http://aUser@www.example.com")]
+        [TestCase("aUser", "aPassword", "http://aUser:aPassword@www.example.com")]
+        [TestCase(null, "aPassword", "http://:aPassword@www.example.com")]
+        public void UsernameAndPassword_ReturnsCorrectUri(string username, string password, string expected)
+        {
+            var uri = FluentUriExtensions.Create()
+                                         .Scheme("http")
+                                         .Host("www.example.com")
+                                         .Username(username)
+                                         .Password(password)
+                                         .AsString();
+
+            uri.Should().Be(expected);
+        }
+
+
+
+        private static IEnumerable GetValidPathSegments()
+        {
+            yield return new object[] {new[] {"any/path/to/resource"}, "http://www.example.com/any/path/to/resource"};
+            yield return new object[] {new[] {"/any/path/to/resource/"}, "http://www.example.com/any/path/to/resource"};
+            yield return new object[] {new[] {"any/path/", "to/resource"}, "http://www.example.com/any/path/to/resource"};
+            yield return new object[] {new[] {"any", "path", "to", "resource"}, "http://www.example.com/any/path/to/resource"};
+        }
+    }
+}
---
+++ FluentUriBuilder/FluentUri.cs
@@ -0,0 +1,213 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Garfoot.FluentUriBuilder
+{
+    public static class FluentUriExtensions
+    {
+        /// <summary>
+        ///     Creates an empty fluent URI context.
+        /// </summary>
+        /// <returns></returns>
+        public static IFluentUriInitial Create()
+        {
+            var context = new FluentUriContext();
+
+            return context;
+        }
+
+        /// <summary>
+        ///     Add a scheme to the URI.
+        /// </summary>
+        /// <param name="uri">The URI.</param>
+        /// <param name="scheme">The scheme to add.</param>
+        /// <returns></returns>
+        public static IFluentUriScheme Scheme(this IFluentUriInitial uri, string scheme)
+        {
+            var context = (FluentUriContext) uri;
+
+            context.Scheme = scheme;
+
+            return context;
+        }
+
+        /// <summary>
+        ///     Add a host to the URI.
+        /// </summary>
+        /// <param name="uri">The URI.</param>
+        /// <param name="host">The host.</param>
+        /// <returns></returns>
+        public static IFluentUriBase Host(this IFluentUriScheme uri, string host)
+        {
+            var context = (FluentUriContext) uri;
+
+            context.Host = host;
+
+            return context;
+        }
+
+        /// <summary>
+        ///     Add one or more path segments to the URI.
+        /// </summary>
+        /// <param name="uri">The URI.</param>
+        /// <param name="segments">The path segments to add.</param>
+        /// <returns></returns>
+        public static IFluentUriBase AddPathSegment(this IFluentUriBase uri, params string[] segments)
+        {
+            return AddPathSegment(uri, (IEnumerable<string>) segments);
+        }
+
+        /// <summary>
+        ///     Add one or more path segments to the URI.
+        /// </summary>
+        /// <param name="uri">The URI.</param>
+        /// <param name="segments">The path segments to add.</param>
+        /// <returns></returns>
+        public static IFluentUriBase AddPathSegment(this IFluentUriBase uri, IEnumerable<string> segments)
+        {
+            var context = (FluentUriContext)uri;
+
+            foreach (string segment in segments)
+            {
+                context.PathSegments.Add(segment.Trim('/'));
+            }
+
+            return context;
+        }
+
+        /// <summary>
+        ///     Sets the port for the URI.
+        /// </summary>
+        /// <param name="uri">The URI.</param>
+        /// <param name="port">The port to use.</param>
+        /// <returns></returns>
+        public static IFluentUriBase Port(this IFluentUriBase uri, int port)
+        {
+            var context = (FluentUriContext)uri;
+
+            context.Port = port;
+
+            return context;
+        }
+
+        /// <summary>
+        ///     Sets the username for the URI.
+        /// </summary>
+        /// <param name="uri">The URI.</param>
+        /// <param name="username">The username.</param>
+        /// <returns></returns>
+        public static IFluentUriBase Username(this IFluentUriBase uri, string username)
+        {
+            var context = (FluentUriContext)uri;
+
+            context.Username = username;
+
+            return context;
+        }
+
+        /// <summary>
+        ///     Sets the password for the URI.
+        /// </summary>
+        /// <param name="uri">The URI.</param>
+        /// <param name="password">The password.</param>
+        /// <returns></returns>
+        public static IFluentUriBase Password(this IFluentUriBase uri, string password)
+        {
+            var context = (FluentUriContext)uri;
+
+            context.Password = password;
+
+            return context;
+        }
+
+        /// <summary>
+        ///     Render a URI as a string.
+        /// </summary>
+        /// <param name="uri">The URI.</param>
+        /// <returns></returns>
+        public static string AsString(this IFluentUriRenderable uri)
+        {
+            var builder = new StringBuilder();
+            var context = (FluentUriContext)uri;
+
+            builder.Append($"{context.Scheme}://");
+
+            bool hasCredentials = false;
+            if (!string.IsNullOrWhiteSpace(context.Username))
+            {
+                builder.Append(context.Username);
+                hasCredentials = true;
+            }
+
+            if (!string.IsNullOrWhiteSpace(context.Password))
+            {
+                builder.Append($":{context.Password}");
+                hasCredentials = true;
+            }
+
+            if (hasCredentials)
+            {
+                builder.Append("@");
+            }
+
+            builder.Append(context.Host);
+
+            if (context.Port.HasValue && context.Port != 80)
+            {
+                builder.Append($":{context.Port}");
+            }
+
+            if (context.PathSegments.Count > 0)
+            {
+                builder.Append("/");
+                builder.Append(string.Join("/", context.PathSegments));
+            }
+
+            return builder.ToString();
+        }
+    }
+
+    /// <summary>
+    ///     A fluent URI in an initial state with no data.
+    /// </summary>
+    public interface IFluentUriInitial
+    {
+    }
+
+    /// <summary>
+    ///     A fluent URI with a scheme defined.
+    /// </summary>
+    public interface IFluentUriScheme
+    {
+    }
+
+    /// <summary>
+    ///     A fluent URI with a base of scheme and host.
+    /// </summary>
+    public interface IFluentUriBase : IFluentUriRenderable
+    {
+    }
+
+    /// <summary>
+    ///     A fluent URI which can be rendered.
+    /// </summary>
+    public interface IFluentUriRenderable
+    {
+    }
+
+    internal class FluentUriContext :
+        IFluentUriInitial,
+        IFluentUriScheme,
+        IFluentUriBase
+    {
+        public string Scheme { get; set; }
+        public string Host { get; set; }
+        public int? Port { get; set; }
+        public IList<string> PathSegments { get; set; } = new List<string>();
+        public IDictionary<string, string> Query { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Fragment { get; set; }
+    }
+}
